347456dc3e6184e130de6fbe565feee9d182ef14
BobFett3185 <kdferdkids@gmail.com>
2024-12-04 16:42:15 +0000

    2nd Commit- updated names of features and fixed output syntax

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,39 +7,65 @@ import statsmodels.api as sm
 model = joblib.load("model.pkl")
 
 # Streamlit app
-st.title("Fintech Project")
-st.write("This app uses a machine learning model to make predictions about the projected price of an Airbnb given its features.")
+st.title("Fintech Real Estate Project 2024")
+st.write("This app uses a machine learning model to make predictions about the price of an Airbnb given its features.")
 
 # User input form
-st.sidebar.header("Input Features")
-feature_1 = st.sidebar.number_input("Feature 1", min_value=0.0, max_value=50.0, value=0.0)
-feature_2 = st.sidebar.number_input("Feature 2", min_value=0.0, max_value=10.0, value=0.0)
-feature_3 = st.sidebar.number_input("Feature 3", min_value=0.0, max_value=10.0, value=0.0)
-feature_4 = st.sidebar.slider("Feature 4", 0.0, 10.0, 0.0)
-feature_5 = st.sidebar.slider("Feature 5", 0.0, 10.0, 0.0)
-feature_6 = st.sidebar.slider("Feature 6", 0.0, 10.0, 0.0)
-feature_7 = st.sidebar.slider("Feature 7", 0.0, 10.0, 0.0)
-feature_8 = st.sidebar.slider("Feature 8", 0.0, 10.0, 0.0)
-feature_9 = st.sidebar.slider("Feature 9", 0.0, 10.0, 0.0)
-feature_10 = st.sidebar.slider("Feature 10", 0.0, 10.0, 0.0)
-feature_11 = st.sidebar.slider("Feature 11", 0.0, 10.0, 0.0)
-feature_12 = st.sidebar.slider("Feature 12", 0.0, 10.0, 0.0)
-feature_13 = st.sidebar.slider("Feature 13", 0.0, 10.0, 0.0)
-feature_14 = st.sidebar.slider("Feature 14", 0.0, 10.0, 0.0)
-feature_15 = st.sidebar.slider("Feature 15", 0.0, 10.0, 0.0)
-feature_16 = st.sidebar.slider("Feature 16", 0.0, 10.0, 0.0)
-feature_17 = st.sidebar.slider("Feature 17", 0.0, 10.0, 0.0)
-feature_18 = st.sidebar.slider("Feature 18", 0.0, 10.0, 0.0)
-feature_19 = st.sidebar.slider("Feature 19", 0.0, 10.0, 0.0)
-feature_20 = st.sidebar.slider("Feature 20", 0.0, 10.0, 0.0)
-feature_21 = st.sidebar.slider("Feature 21", 0.0, 10.0, 0.0)
-feature_22 = st.sidebar.slider("Feature 22", 0.0, 10.0, 0.0)
-feature_23 = st.sidebar.slider("Feature 23", 0.0, 10.0, 0.0)
-feature_24 = st.sidebar.slider("Feature 24", 0.0, 10.0, 0.0)
-feature_25 = st.sidebar.slider("Feature 25", 0.0, 10.0, 0.0)
-feature_26 = st.sidebar.slider("Feature 26", 0.0, 10.0, 0.0)
-feature_27 = st.sidebar.slider("Feature 27", 0.0, 10.0, 0.0)
-feature_28 = st.sidebar.slider("Feature 28", 0.0, 10.0, 0.0)
+st.sidebar.header("Input Features Of the Property")
+feature_1 = st.sidebar.number_input("Number of Reviews", min_value=0.0, max_value=50.0, value=0.0)
+feature_2 = st.sidebar.number_input("Number of Beds", min_value=0.0, max_value=10.0, value=0.0)
+choice = st.sidebar.radio("Cleaning Fee?", ["No", "Yes"])
+feature_3 = 1 if choice == "Yes" else 0
+choice = st.sidebar.radio("Apartment?", ["No", "Yes"])
+feature_4 = 1 if choice == "Yes" else 0
+choice = st.sidebar.radio("Bed and Breakfast?", ["No", "Yes"])
+feature_5 = 1 if choice == "Yes" else 0
+choice = st.sidebar.radio("Boutique Hotel ?", ["No", "Yes"])
+feature_6 = 1 if choice == "Yes" else 0
+choice = st.sidebar.radio("Bungalow?", ["No", "Yes"])
+feature_7 = 1 if choice == "Yes" else 0
+choice = st.sidebar.radio("Cabin?", ["No", "Yes"])
+feature_8 = 1 if choice == "Yes" else 0
+choice = st.sidebar.radio("Camper/RV?", ["No", "Yes"])
+feature_9 = 1 if choice == "Yes" else 0
+choice = st.sidebar.radio("Condo?", ["No", "Yes"])
+feature_10  = 1 if choice == "Yes" else 0
+choice = st.sidebar.radio("Dorm?", ["No", "Yes"])
+feature_11  = 1 if choice == "Yes" else 0
+choice = st.sidebar.radio("Guest Suite?", ["No", "Yes"])
+feature_12 = 1 if choice == "Yes" else 0
+choice = st.sidebar.radio("Guesthouse?", ["No", "Yes"])
+feature_13 = 1 if choice == "Yes" else 0
+choice = st.sidebar.radio("Hostel", ["No", "Yes"])
+feature_14 = 1 if choice == "Yes" else 0
+choice = st.sidebar.radio("Inlaw?", ["No", "Yes"])
+feature_15 = 1 if choice == "Yes" else 0
+choice = st.sidebar.radio("Loft?", ["No", "Yes"])
+feature_16 = 1 if choice == "Yes" else 0
+choice = st.sidebar.radio("Other?", ["No", "Yes"])
+feature_17  = 1 if choice == "Yes" else 0
+choice = st.sidebar.radio("Timeshare?", ["No", "Yes"])
+feature_18 = 1 if choice == "Yes" else 0
+choice = st.sidebar.radio("Townhouse?", ["No", "Yes"])
+feature_19 = 1 if choice == "Yes" else 0
+choice = st.sidebar.radio("Villa?", ["No", "Yes"])
+feature_20 = 1 if choice == "Yes" else 0
+choice = st.sidebar.radio("Couch?", ["No", "Yes"])
+feature_21 = 1 if choice == "Yes" else 0
+choice = st.sidebar.radio("Futon?", ["No", "Yes"])
+feature_22 = 1 if choice == "Yes" else 0
+choice = st.sidebar.radio("Pull out Sofa?", ["No", "Yes"])
+feature_23 = 1 if choice == "Yes" else 0
+choice = st.sidebar.radio("Is it in Chicago?", ["No", "Yes"])
+feature_24 = 1 if choice == "Yes" else 0
+choice = st.sidebar.radio("Is it in DC?", ["No", "Yes"])
+feature_25= 1 if choice == "Yes" else 0
+choice = st.sidebar.radio("Is it in LA?", ["No", "Yes"])
+feature_26 = 1 if choice == "Yes" else 0
+choice = st.sidebar.radio("Is it in San Fransisco?", ["No", "Yes"])
+feature_27  = 1 if choice == "Yes" else 0
+choice = st.sidebar.radio("Is the property instantly bookable?", ["No", "Yes"])
+feature_28= 1 if choice == "Yes" else 0
 # Prepare data for prediction
 
 input_data = np.array([[feature_1, feature_2, feature_3, feature_4,feature_5,feature_6,feature_7,feature_8, 
@@ -47,11 +73,13 @@ input_data = np.array([[feature_1, feature_2, feature_3, feature_4,feature_5,fea
                         feature_18, feature_19, feature_20, feature_21, feature_22, feature_23, feature_24, feature_25, 
                         feature_26, feature_27, feature_28]])
 
-# Make predictions
-import math
 
+
+
+import math
 if st.button("Predict"):
     prediction = model.predict(input_data)
-    
-    st.success(f"The predicted value is: {prediction[0]}")
+    prediction = math.exp(prediction)
+    roundedPrediction = round(prediction,2)
+    st.success(f"The predicted value is: ${roundedPrediction}")
 
